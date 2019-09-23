4b5e1829e3e506befecfb23ccd52dfeb3cf95fcf
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2019-09-23 17:25:41 -0500

    Create OutputArrayContents.java

---
+++ OutputArrayContents.java
@@ -0,0 +1,16 @@
+/**
+ * Output the contents of an array.
+ */
+
+public class OutputArrayContents {
+
+    public static void main(String[] args) {
+
+        int[] myArray = {10, 20, 30, 40, 50};
+
+        // Output the contents of the array
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+    }
+}
