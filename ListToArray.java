39566b69c563aa04525509798966984ec0621920
Robert Griffiths <34591246+robertgriffiths@users.noreply.github.com>
2019-11-26 11:13:14 -0500

    Create ListToArray.java

---
+++ ListToArray.java
@@ -0,0 +1,32 @@
+/*
+ * Lists to array
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListToArray {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(12);
+        numbers.add(13);
+        numbers.add(28);
+        numbers.add(94);
+        numbers.add(43);
+
+        int[] arrayNumbers = new int[numbers.size()];
+
+        // populate array with contents of List
+        for(int i = 0; i < numbers.size(); i++) {
+            arrayNumbers[i] = numbers.get(i);
+        }
+
+        // output contents of array
+        for (int i : arrayNumbers)
+            System.out.println(i);
+    }
+
+}
